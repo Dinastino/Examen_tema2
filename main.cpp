468a52be0d4b6db0741ab97b21877d7f3d2b7e7e
Carlos <corammun@myuax.com>
2023-12-14 12:08:56 +0100

    1fetfg

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 
 struct Estudiante{
-    st nombre;
+    string nombre;
     int edad;
     float pormedio;
+
 };
 
 void mostrarEstudiante(){
     struct Estudiante estudiante1;
-    estudiante1.nombre = 'Juan';
+    estudiante1.nombre = "Juan";
     estudiante1.edad = 12;
     estudiante1.pormedio = 9.4;
     cout << "Nombre: " << estudiante1.nombre<< "\nEdad: " << estudiante1.edad<< "\nPromedio: " << estudiante1.pormedio << endl;
