c759dfbac5ef4ff092d49c809a72272e4fe61854
Carlos <corammun@myuax.com>
2023-12-14 12:27:10 +0100

    No entiendo porq falla

--- main.cpp
+++ main.cpp
@@ -13,15 +13,21 @@ void mostrarEstudiante(){
     struct Estudiante estudiante1;
     estudiante1.nombre = "Juan";
     estudiante1.edad = 12;
-    estudiante1.pormedio = 9.4;
+    estudiante1.pormedio = 5.1;
     cout << "Nombre: " << estudiante1.nombre<< "\nEdad: " << estudiante1.edad<< "\nPromedio: " << estudiante1.pormedio << endl;
 }
 
 void mostrarMaterias(){
+    struct Estudiante estudiante1;
     string mat;
     int i;
-    for (i=0,i<10,i++){
+    cout << "Escriba las Asignaturas/Materias una por una:" << endl;
+    for(i=0,i<10,i++){
         cin >> mat;
+        estudiante1.materias.push_back(mat);
+    }
+    for (const auto& materia : estudiante1.materias) {
+        cout << materia << " ";
     }
 }
 
