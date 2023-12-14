dea9df7e549089ca9b192ee2534a112de00990bd
Carlos <corammun@myuax.com>
2023-12-14 11:51:59 +0100

    1fedsf

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct Estudiante{
+    char nombre;
+    int edad;
+    float pormedio;
+    void mostrat_estudiante(){
+        struct Estudiante estudiante1;
+    }
+};
+
+void mostrat_estudiante(){
+    struct Estudiante estudiante1;
+}
